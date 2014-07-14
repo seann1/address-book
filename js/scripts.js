46b64a15e2ba72e9aaa05865485e2bf8a2fc7bb9
Jeff Stringer & Michael Derfler <student@epicodus.com>
2014-07-14 16:58:10 -0700

    added tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,42 @@
+// $(document).ready(function(){
+//   $("form#task-list").submit(function(event){
+//     event.preventDefault();
+//     $("#add-task").click(function() {
+//       $("#new-task").append('<div class="new-task">' +
+//                                  '<div class="form-group">' +
+//                                    '<label for="new-street">New Task</label>' +
+//                                    '<input type="text" class="form-control new-task">' +
+//                                  '</div>' );
+//     });
+//   });
+// });
+
+
 $(document).ready(function(){
   $("form#task-list").submit(function(event){
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedFirstTask = $("input#new-first-task").val();
+    var inputtedList = $("input#new-list").val();
+    var inputtedTask = $("input#new-tasks").val();
+    var newList = { list: inputtedList, tasks: []};
 
-    var newWorker = { firstName: inputtedFirstName, lastName: inputtedLastName, firstTask: inputtedFirstTask };
-    var Worker = [];
-    Worker.push(newWorker);
-    console.log(Worker);
-    $("ul#tasks").append("<li><span class='task'>" + newWorker.firstName + " " + newWorker.lastName + " " + newWorker.firstTask + "</span></li>");
-    //console.log(newWorker);
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-first-task").val("");
+    $("ul#list").append("<li><span class='list'>" + newList.list + "</span></li>");
+    $("input#new-list").val("");
+    $("input#new-tasks").val("");
 
-    $(".task").last().click(function() {
-      //console.log(newWorker);
+    $(".list").last().click(function() {
+      $("#show-lists").show();
+      $("#show-lists h2").text(newList.list);
+    });
 
-      $("#show-tasks").show();
-      $("#show-tasks h2").text(newWorker.firstName + " " + newWorker.lastName);
-      $(".first-name").text(newWorker.firstName);
-      $(".last-name").text(newWorker.lastName);
-      $(".tasked").text(newWorker.firstTask);
+    $("#add-tasks").click(function() {
+      var inputtedTask = $("input#new-tasks").val();
+      var newTask = { description: inputtedTask };
+      newList.tasks.push(newTask);
+      console.log(newList.tasks);
+      newList.tasks.forEach(function(task) {
+        $("ul#tasks").append("<li><span class='task'>" + task.description + "</li></span>");
+      });
     });
   });
 });
