57e25b91bcf1c20ae8dc6572e24bcd7270e82b74
Jeff Stringer & Sean Niesen <student@epicodus.com>
2014-07-15 16:40:16 -0700

    ready to correct tasks

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,13 @@ $(document).ready(function(){
     var name = $("input#new-list").val();
     var inputtedTask = $("input#new-tasks").val();
     var newList = { list: name, tasks: []};
-    //var currentList = newList;
+    //var currentList = Object.create(newList);
 
+    // This is the list of created lists:
     $("ul#list").append("<li><span class='list'>" + newList.list + "</span></li>");
     $("input#new-list").val("");
-    //$("input#new-tasks").val("");
 
+    // Clicking on the .list class should display the tasks for this list
     $(".list").last().click(function() {
       $("#show-lists").show();
       $("#show-lists h2").text(newList.list);
@@ -21,12 +22,13 @@ $(document).ready(function(){
       console.log(newList.tasks);
     });
 
+    //Clicking on this submit button should display the tasks for
     $("form#add-tasks").submit(function(event){
       event.preventDefault();
       var inputtedTask = $("input#new-tasks").val();
       var newTask = { description: inputtedTask };
-      $("ul#tasks").empty();
       newList.tasks.push(newTask);
+      $("ul#tasks").empty();
       newList.tasks.forEach(function(task) {
         $("ul#tasks").append("<li><span class='task'>" + task.description + "</li></span>");
       });
