474a78b5c95e3a61d9234ee3967d4e8f48e9c7d6
Jeff Stringer & Sean Niesen <student@epicodus.com>
2014-07-15 13:34:18 -0700

    to do list needs task differentiation

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,31 @@ $(document).ready(function(){
   $("form#task-list").submit(function(event){
     event.preventDefault();
 
-    var inputtedList = $("input#new-list").val();
+    var name = $("input#new-list").val();
     var inputtedTask = $("input#new-tasks").val();
-    var newList = { list: inputtedList, tasks: []};
+    var newList = { list: name, tasks: []};
+    //var currentList = newList;
 
     $("ul#list").append("<li><span class='list'>" + newList.list + "</span></li>");
     $("input#new-list").val("");
-    $("input#new-tasks").val("");
+    //$("input#new-tasks").val("");
 
     $(".list").last().click(function() {
       $("#show-lists").show();
       $("#show-lists h2").text(newList.list);
+      $("ul#tasks").empty();
+      newList.tasks.forEach(function(task) {
+        $("ul#tasks").append("<li><span class='task'>" + task.description + "</li></span>");
+      });
+      console.log(newList.tasks);
     });
 
-    /*
-    $(".new-jobs").each(function(){
-      var inputtedTask = $(this).find("input#new-tasks").val();
-      var newTask = { description: inputtedTask };
-      newList.tasks.push(newTask);
-    });
-    */
-
-    $("#add-tasks").last().click(function() {
+    $("form#add-tasks").submit(function(event){
+      event.preventDefault();
       var inputtedTask = $("input#new-tasks").val();
       var newTask = { description: inputtedTask };
+      $("ul#tasks").empty();
       newList.tasks.push(newTask);
-      $("ul#tasks").text("");
       newList.tasks.forEach(function(task) {
         $("ul#tasks").append("<li><span class='task'>" + task.description + "</li></span>");
       });
